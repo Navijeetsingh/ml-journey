d36063208118dca9130d54e3fa9074de0991cde5
navijeetsingh <navijeet.singh.77770in@gmail.com>
2026-04-06 17:29:13 +0530

    Day 3 - list and loops

---
+++ day3.py
@@ -0,0 +1,13 @@
+fruits=["apple","banana","mango"]
+for fruit in fruits:
+    print(fruit)
+numbers=[10,20,30,40,50]
+for number in numbers:
+    print(number)
+for i in range(5):
+    print(i)
+for b in range(1,11):
+    print(b)
+o=list(range(10))
+print(o)
+
