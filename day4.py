69d5b4c355350ff23ab63db7fcb308e353295b13
Navijeetsingh <navijeet.singh.77770in@gmail.com>
2026-06-08 14:09:45 +0530

    Day 4 - logical operators

---
+++ day4.py
@@ -0,0 +1,43 @@
+#is_student=True
+#is_working=False
+#print(is_student)
+#print(is_working)
+#print(5 > 3)
+#print(5 < 3)
+#print(5 == 5)
+#print(5 ==3)
+#print(5 != 3)
+#print(10>=10)
+#age=20
+#if age >= 18:
+#    print("You are an adult.")
+#elif age >= 13:
+#    print("You are a teenager.")
+#else:
+#    print("You are a minor.")
+#test_score = 85
+#if test_score >= 90:
+#    print("grade A")
+#elif test_score >= 80:
+#    print("grade B")
+#elif test_score >= 70:
+#    print("grade C")
+#else:
+#    print("grade F")
+#age=20
+#has_id=False
+#if age >= 18 and has_id == True:
+#    print("You can enter.")
+#else:
+#    print("You cannot enter.")
+#has_ticket=False
+#is_vip=False
+#if has_ticket or is_vip:
+#    print("You can enter.")
+#else:
+#    print("You cannot enter.")
+is_raining=False
+if not is_raining:
+    print("go outside.")
+else:
+    print("stay inside.")
