0fd430fa006aa69bb465aebfcf9fb1d52cbde276
Navijeetsingh <navijeet.singh.77770in@gmail.com>
2026-06-09 13:04:50 +0530

    Day 5 - while loops, input, functions, return

---
+++ day5.py
@@ -0,0 +1,38 @@
+#count=1
+#while count<=5:
+#    print(count)
+#    count+=1
+#name=input("what is your name? ")
+#print(f"Hello, {name}")
+#password="python123"
+#guess=""
+#while guess!=password:
+#    guess=input("Enter the password: ")
+#print("Correct password!")
+#def greet(name):
+#    print(f"hello {name}!")
+#greet("Navi")
+#greet("ALi")
+#greet("Sara")
+#def add(a,b):
+#    return a+b
+#result=add(5,3)
+#print(result+2)
+#def add(a,b):
+#    print(a+b)
+#add(5,3)
+#result=add(5,3)
+#def add_print(a,b):
+#    print(a+b)
+#def add_return(a,b):
+#    return a+b
+#add_print(5,3)
+#x=add_return(5,3)
+#print(x+10)
+def multiply(a,b):
+    return a*b
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+result=multiply(a,b)
+print(result)
+
