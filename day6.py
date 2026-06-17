d39b62599902596510e119f790cb0fb11d12179e
Navijeetsingh <navijeet.singh.77770in@gmail.com>
2026-06-17 09:00:43 +0530

    Day 7 - string methods

---
+++ day6.py
@@ -0,0 +1,45 @@
+#person={
+#    "name": "navi",
+#    "age": 18,
+#    "city": "punjab"
+#}
+#print(person["name"])
+#print(person["age"])
+#person["email"]="navi@gmail.com"
+#person["age"]=19
+#del person["city"]
+#print(person.keys())
+#print(person.values())
+#print(person.items())
+#for key, value in person.items():
+#    print(f"{key}: {value}")
+#person={
+#    "name": "navi",
+#    "age": 18,
+#    "grade":"B.Sc Mathmatics",
+#    "score": 80,
+#}
+#print(person["name"])
+#print(person["score"])
+#person["score"]=90
+#for key, value in person.items():
+#    print(f"{key}: {value}")
+#for x in person:
+#    print(f"{x}: {person[x]}")
+#person={"name": "navi","age": 18}
+#person["email"]="navi@gmail.com"
+#if "email" in person:
+#    print(person["email"])
+#else:
+#    print("Email not found.")
+name="hello world"
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+text="   hello world   "
+print(text.strip())
+print(len(text))
+print(len(text.strip()))
+sentence="I love Python"
+print(sentence.replace("Python", "ML"))
+print(sentence.split())
