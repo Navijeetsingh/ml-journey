2b7913b4d56c5b606e916fc5dfdb6d1d0c84962f
Navijeetsingh <navijeet.singh.77770in@gmail.com>
2026-06-19 12:05:28 +0530

    Day 7 -string methods, tuples and sets

---
+++ day7.py
@@ -0,0 +1,24 @@
+#text = "Hello, World"
+#print(text.find("World"))
+#print(text.count("l"))
+#print(text.startswith("Hello"))
+#print(text.endswith("World"))
+#print(text.startswith("World"))
+#words=["I", "love", "Python"]
+#sentence = "-".join(words)
+#print(sentence)
+#coordinates = (10, 20)
+#coordinates[0] = 99
+#colors = ("red", "green", "blue")
+#print(coordinates)
+#print(colors[0])
+numbers = {1, 2, 3, 3,2,1}
+print(numbers)
+names=["navi","ali","navi","sara","ali"]
+unique_names = list(set(names))
+print(unique_names)
+a={1,2,3,4}
+b={3,4,5,6}
+print(a&b)
+print(a|b)
+print(a-b)
